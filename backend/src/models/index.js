cd4a953382ed7126874120d9586d7cb3d3592ce5
NISHANTKHARGA <nishantkharga00@gmail.com>
2026-05-27 23:21:46 +0545

    Always load models regardless of sequelize

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -4,12 +4,12 @@ const sequelize = conn.sequelize;
 let User, Lawyer, Appointment, ChatMessage;
 
 try {
-  if (sequelize) {
-    User = require('./User');
-    Lawyer = require('./Lawyer');
-    Appointment = require('./Appointment');
-    ChatMessage = require('./ChatMessage');
+  User = require('./User');
+  Lawyer = require('./Lawyer');
+  Appointment = require('./Appointment');
+  ChatMessage = require('./ChatMessage');
 
+  if (sequelize) {
     User.hasMany(Appointment, { foreignKey: 'userId', as: 'appointments' });
     Appointment.belongsTo(User, { foreignKey: 'userId', as: 'user' });
 
@@ -19,7 +19,7 @@ try {
     User.hasMany(ChatMessage, { foreignKey: 'userId', as: 'messages' });
     ChatMessage.belongsTo(User, { foreignKey: 'userId', as: 'user' });
   } else {
-    console.warn('Sequelize is null, models not defined');
+    console.warn('Sequelize is null, associations not defined');
   }
 } catch (e) {
   console.error('Model definition error:', e?.message);
